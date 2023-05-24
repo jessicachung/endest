425fb112ffdc66a2fac4febd28d48a0868c3f784
Jessica Chung <jchung@unimelb.edu.au>
2023-05-24 13:39:13 +1000

    Add data files and docs

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Example RNA-seq data
+#'
+#' Example RNA-seq count data of 3 samples and 2000 genes.
+#'
+#' @format A matrix with 3 columns and 2000 rows.
+"example_rna_exprs"
+
+#' Example array data
+#'
+#' Example Illumina microarray expression data of 3 samples and 5000 probes.
+#'
+#' @format A matrix with 3 columns and 5000 rows.
+"example_array_exprs"
+
+#' Example Entrez IDs
+#'
+#' List of probe IDs to Entrez IDs corresponding to the 5000 probes in the example_array_exprs dataset.
+#'
+#' @format A vector of length 5000 containing Entrez IDs.
+"example_array_entrez_ids"
